42a30393712d7e69f35a1750677149411cf03e14
Mide Victor <midevictor90@gmail.com>
2023-03-02 17:21:19 +0100

    created the listing component: users can see all condos available for rent or sale

---
+++ src/pages/Category.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+//to check if it is for sale or rent, import useParams
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
+
+function Category() {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        //Get a Reference
+        const listingsRef = collection(db, "listings");
+
+        //create a query
+        const q = query(
+          listingsRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        //exceute the query
+        const querySnap = await getDocs(q);
+        const listing = [];
+        querySnap.forEach((doc) => {
+          return listing.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setListings(listing);
+        setLoading(false);
+      } catch (error) {
+        toast.error("could  not fetch listings");
+      }
+    };
+    fetchListings();
+  }, [params.categoryName]);
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">
+          {params.categoryName === "rent"
+            ? "Places for rent"
+            : "Places for sale"}
+        </p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+        <main>
+          <ul className="categoryListings">
+            {listings.map((listing) => (
+              <ListingItem key={listing.id} listing={listing.data} id={listing.id} />  
+            ))}
+          </ul>
+        </main>
+        </>
+      ) : (
+        <p>No listings for {params.categoryName} </p>
+      )}
+    </div>
+  );
+}
+
+export default Category;
+//fetching the listing from firebase and display them here
